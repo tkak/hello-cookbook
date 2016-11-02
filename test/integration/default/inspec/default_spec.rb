20b6ec8f404a5c302360c6707e2c4948fbcffe52
Takaaki Furukawa <takaaki.frkw@gmail.com>
2016-11-02 09:08:10 +0900

    Use inspec

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -2,5 +2,5 @@ describe file('/usr/local/foo') do
   it { should be_directory }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
-  it { should be_mode 755 }
+  its('mode') { should cmp '0755' }
 end
