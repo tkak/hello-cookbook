20e65af263c098a8f789d6591b5b6b414ec73ec4
Takaaki Furukawa <takaaki.frkw@gmail.com>
2016-11-02 12:14:59 +0900

    Improve test

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,8 +3,19 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'zsh' do
+  action :install
+end
+
 directory '/usr/local/foo' do
   owner 'root'
   group 'root'
   mode '755'
+end
+
+file '/usr/local/foo/bar' do
+  content 'Hello'
+  owner 'root'
+  group 'root'
+  mode '644'
 end
